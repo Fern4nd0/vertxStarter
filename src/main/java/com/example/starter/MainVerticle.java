ab2c75017068c60f7a1c427d2d050c82044a761f
fernandopmen <fernandopmen@ext.zara.com>
2021-03-17 18:58:51 +0100

    add main verticle

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -1,13 +1,29 @@
 package com.example.starter;
 
-import io.vertx.core.AbstractVerticle;
+import com.example.starter.handler.UserHandler;
+import io.reactivex.Single;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.config.ConfigRetriever;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.core.http.HttpServer;
+import io.vertx.reactivex.ext.mongo.MongoClient;
+import io.vertx.reactivex.ext.web.Router;
+import com.example.starter.repository.UserRepository;
+import com.example.starter.router.UserRouter;
+import com.example.starter.service.UserService;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
 
+    final ConfigStoreOptions store = new ConfigStoreOptions().setType("env");
+    final ConfigRetrieverOptions options = new ConfigRetrieverOptions().addStore(store);
+    final ConfigRetriever retriever = ConfigRetriever.create(vertx, options);
     vertx.createHttpServer().requestHandler(req -> {
       req.response()
         .putHeader("content-type", "text/plain")
@@ -21,4 +37,25 @@ public class MainVerticle extends AbstractVerticle {
       }
     });
   }
+
+  // Private methods
+  private MongoClient createMongoClient(Vertx vertx, JsonObject configurations) {
+    final JsonObject config = new JsonObject()
+      .put("host", configurations.getString("HOST"))
+      .put("username", configurations.getString("USERNAME"))
+      .put("password", configurations.getString("PASSWORD"))
+      .put("authSource", configurations.getString("AUTHSOURCE"))
+      .put("db_name", configurations.getString("DB_NAME"))
+      .put("useObjectId", true);
+
+    return MongoClient.createShared(vertx, config);
+  }
+
+  private Single<HttpServer> createHttpServer(Router router, JsonObject configurations) {
+    return vertx
+      .createHttpServer()
+      .requestHandler(router)
+      .rxListen(configurations.getInteger("HTTP_PORT"));
+  }
+
 }
